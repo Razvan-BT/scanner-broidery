69acc9d2ebf9b35cfb70826b62abc28ed3da0703
Razvan-BT <spatarurazvan8@gmail.com>
2022-10-10 15:45:07 +0300

    tot

--- src/qr-scan.js
+++ src/qr-scan.js
@@ -1,21 +1,88 @@
-function onScanSuccess(decodedText, decodedResult) {
-    // handle the scanned code as you like, for example:
-    console.log(`Code matched = ${decodedText}`, decodedResult);
-}
 
-// Square QR box with edge size = 70% of the smaller edge of the viewfinder.
-let qrboxFunction = function(viewfinderWidth, viewfinderHeight) {
-    let minEdgePercentage = 0.7; // 70%
-    let minEdgeSize = Math.min(viewfinderWidth, viewfinderHeight);
-    let qrboxSize = Math.floor(minEdgeSize * minEdgePercentage);
-    return {
-        width: qrboxSize,
-        height: qrboxSize
-    };
-}
+$(document).ready(function () {
+    let vteId = "";
+    let lastItemScanned;
+    let statusScann = false;
 
-let html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader",
-    { fps: 10, qrbox: qrboxFunction },
-    /* verbose= */ false);
-html5QrcodeScanner.render(onScanSuccess);
+    console.warn("Page loaded!")
+    function onScanSuccess(decodedText, decodedResult) {
+        console.log(`Scan result ${decodedText}`, decodedResult);
+        vteId = decodedText;
+        if (vteId != lastItemScanned) {
+            if ($('#searchbar').val().length <= 0) {
+                if(statusScann == false){    
+                    $('#searchbar').val(vteId)
+                    lastItemScanned = vteId;
+                    alert(decodedText)
+                    checkDataFrom__();
+                    html5qrcode.stop();
+                }
+            }
+        
+        } else {
+            statusScann = false;
+            alert(`You already scanned this VTE! ${lastItemScanned} VTE ${vteId}`)
+        }
+        // console.log('decodedText:', decodedText);
+    }
+    let html5qrcode = new Html5Qrcode("qr-reader", {
+        // Use this flag to turn on the feature.
+        experimentalFeatures: {
+            useBarCodeDetectorIfSupported: true
+        },
+        rememberLastUsedCamera: true,
+        formatsToSupport: [Html5QrcodeSupportedFormats.QR_CODE, Html5QrcodeSupportedFormats.CODE_39],
+
+
+    });
+    const scanConfig = { fps: 1, qrbox: 600 };
+    // If you want to prefer front camera
+
+
+    if (statusScann) {
+        statusScann = false;
+        html5qrcode.stop();
+        html5qrcode.clear();
+    } else {
+        html5qrcode.start({ facingMode: "environment" }, scanConfig, onScanSuccess);
+    }
+
+
+    function checkDataFrom__() {
+
+        if (vteId.length > 0 && vteId != "") 
+        {
+            if($('#searchbar').val().length > 0) 
+            {
+                $('#searchbar').val(vteId);
+                alert("checkDataFrom_ called");
+                alert(`VTE ${vteId} in ${takeValFromInput}`)
+                TakeDataFromNode(vteId)
+                $('#btnsearch').click();
+            }
+        }
+    }
+
+
+
+    function TakeDataFromNode(input) {
+        $('#btnsearch').on('click', function () {
+            alert("btn was clicked")
+            $('#searchbar').val("");
+            vteId = "";
+            statusScann = false;
+            // let params = "http://127.0.0.1:1880/broidery_vte?v="+decodedText;
+            let params = "https://node.formens.ro/broidery_vte?v=" + input
+            fetch(params)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    alert(data)
+                })
+        })
+
+    }
+
+});
+
